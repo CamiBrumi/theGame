0a79a129479f2d40053ea75151486ec0cb0abaf0
Cami <cameliabrumar@gmail.com>
2017-12-03 02:23:45 +0100

    He modificat el jugador Kami, continua tenint errors. He treballat amb la funció move

--- AIKami.cc
+++ AIKami.cc
@@ -8,19 +8,23 @@
 #define PLAYER_NAME Kami
 
 
-struct PLAYER_NAME : public Player {
+/**
+ * Factory: returns a new instance of this class.
+ * Do not modify this function.
+ */
+ struct PLAYER_NAME : public Player {
 
-  /**
-   * Factory: returns a new instance of this class.
-   * Do not modify this function.
-   */
-  static Player* factory () {
-    return new PLAYER_NAME;
-  }
+   /**
+    * Factory: returns a new instance of this class.
+    * Do not modify this function.
+    */
+   static Player* factory () {
+     return new PLAYER_NAME;
+   }
 
-  /**
-   * Types and attributes for your player can be defined here.
-   */
+   /**
+    * Types and attributes for your player can be defined here.
+    */
 
    typedef vector<int> VI;
    typedef vector<VI>  VVI;
@@ -28,43 +32,67 @@ struct PLAYER_NAME : public Player {
    // Stores the location of orks.
    VVI ork_at;
 
-   // Moves ork with identifier id.
-   void move(int id) {
-     Dir dir = Dir(RIGHT);
-     //Pos npos = unit(id).pos + dir;
-     execute(Command(id, dir));
-
-     /*
-     Unit u = unit(id);
-     Pos pos = u.pos;
-     // Try to move to a position within the board.
-     for (int d = 0; d != DIR_SIZE; ++d) {
-       Dir dir = Dir(d);
-       Pos npos = pos + dir;
-       if (pos_ok(npos) and cell(npos).type != WATER) {
-         execute(Command(id, dir));
-         return;
-       }
-     }
-     */
+   // Returns true if winning.
+   bool winning() {
+     for (int pl = 0; pl < nb_players(); ++pl)
+       if (pl != me() and total_score(me()) <= total_score(pl))
+         return false;
+     return true;
    }
 
-  /**
-   * Play method, invoked once per each round.
-   */
-  virtual void play () {
-    VI my_orks = orks(me()); // Get the id's of my orks.
+   Pos trobaCasellaProfitable(set<Pos>& posicionsVisitades, Pos posActual, Dir& dirRetornada) {
+     Pos retPos;
+     Cell tipusCasellaActual = Cell(posActual).type;
+     if ((tipusCasellaActual == CITY or tipusCelaActual == PATH) and unit(Cell(posActual).unit_id).player != me()) retPos = posActual;
+     else if (tipusCasellaActual == WATER or !pos_ok(posActual) or posicionsVisitades.find(posActual) != posicionsVisitades.end()) {
+       retPos = Pos(-1, -1); // si es una posicio visitada, no tenim que anar cap a ella (ens bloquejariem en un bucle infinit). en canvi, si l'iterador és == .end del set => no hem visitat aquesta posicio
+       posicionsVisitades.insert(posActual);
+     }
+     else {
+       posicionsVisitades.insert(posActual);
+       bool pos_trobada = false;
+       for (int d = 0; d != DIR_SIZE and !pos_trobada; ++d) {
+         Dir dir = Dir(d);
+         Pos npos = posActual + dir;
+         posicionsVisitades.insert(npos);
+         Dir dummyDir;
+         retPos = trobaCasellaProfitable(posicionsVisitades, npos, dummyDir);
+         dirRetornada = dir;
+         if (pos_ok(retPos)) pos_trobada = true;
+       }
+     }
+     return retPos;
 
-    // Process orks in random order.
-    VI perm = random_permutation(my_orks.size());
-    for (int k = 0; k < int(perm.size()); ++k)
-      move(my_orks[perm[k]]);
-  }
+   }
 
-};
+   // Moves ork with identifier id.
+   void move(int id) {
+     set<Pos> posicionsVisitades;
+     Unit u = unit(id);
+     Pos posActual = u.pos;
+     Dir dirCasellaProf = NONE;
+     Pos posCasellaProf = trobaCasellaProfitable(posicionsVisitades, posActual, dirCasProf);
+     Dir dir = Dir(dirCasellaProf);
+     Pos npos = posActual + dir;
+     execute(Command(id, dir));
+   }
+
+   /**
+    * Play method, invoked once per each round.
+    */
+    virtual void play () {
+      VI my_orks = orks(me()); // Get the id's of my orks.
+
+      // Process orks in random order.
+      VI perm = random_permutation(my_orks.size());
+      for (int k = 0; k < int(perm.size()); ++k)
+        move(my_orks[perm[k]]);
+    }
+
+ };
 
 
-/**
- * Do not modify the following line.
- */
-RegisterPlayer(PLAYER_NAME);
+ /**
+  * Do not modify the following line.
+  */
+ RegisterPlayer(PLAYER_NAME);
